00cf3ae707ac44c427b0a99bcdfb653ca5cdab25
Geraldo Fada <geraldofada.neto@gmail.com>
2019-11-20 00:06:36 -0300

    Removed common

--- common/common.h
+++
@@ -1,9 +0,0 @@
-#ifndef HUFFMAN_CRYPTO_COMMON_H
-#define HUFFMAN_CRYPTO_COMMON_H
-
-typedef struct rune {
-    int prob;
-    char symbol;
-} cmn_Rune;
-
-#endif //HUFFMAN_CRYPTO_COMMON_H
--- main.c
+++ main.c
@@ -1,72 +1,23 @@
 #include <stdio.h>
 
-#include "huffman-tree/minheap.h"
+#include "huffman-tree/huffman-tree.h"
 
-void pp(hfm_Minheap *mh) {
-    for (int i = 0; i <= mh->tail; i++) {
-        printf("%i:[%i, %c] - ", i, mh->data[i].prob, mh->data[i].symbol);
+void imprime_aux(hfm_Node* a, int andar){
+    if(a){
+        int j;
+        imprime_aux(a->left, andar + 1);
+        for(j = 0; j <= andar; j++) printf("   ");
+        printf("[%i, %c]\n", a->prob, a->symbol);
+        imprime_aux(a->right, andar + 1);
     }
-    printf(" t: %i\n", mh->tail);
+}
+
+void imprime_ab(hfm_Node* a){
+    imprime_aux(a, 1);
 }
 
 int main() {
-    hfm_Minheap *mh = hfm_Create_Minheap(2);
-    hfm_T_Minheap in = {100, 'a'};
+    hfm_
 
-    hfm_Insert_Minheap(mh, in);
-
-    in.prob = 8;
-    hfm_Insert_Minheap(mh, in);
-
-    in.prob = 12;
-    hfm_Insert_Minheap(mh, in);
-
-    in.prob = 1;
-    hfm_Insert_Minheap(mh, in);
-
-    in.prob = 9;
-    hfm_Insert_Minheap(mh, in);
-
-    in.prob = 99;
-    hfm_Insert_Minheap(mh, in);
-
-    pp(mh);
-
-    hfm_Pop_Minheap(mh);
-
-    pp(mh);
-
-    in.prob = 88;
-    hfm_Insert_Minheap(mh, in);
-
-    pp(mh);
-
-    in.prob = -1;
-    hfm_Insert_Minheap(mh, in);
-
-    pp(mh);
-
-    in.prob = -10;
-    hfm_Insert_Minheap(mh, in);
-
-    pp(mh);
-
-    printf("Full? %i\n", hfm_Is_Full_Minheap(mh));
-
-    in.prob = -10;
-    hfm_Insert_Minheap(mh, in);
-
-    pp(mh);
-
-    printf("Full? %i\n", hfm_Is_Full_Minheap(mh));
-
-    hfm_Pop_Minheap(mh);
-    hfm_Pop_Minheap(mh);
-
-
-    printf("Empty? %i\n", hfm_Is_Empty_Minheap(mh));
-
-
-    hfm_Destroy_Minheap(mh);
     return 0;
 }
