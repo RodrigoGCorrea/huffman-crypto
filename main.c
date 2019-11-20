4c667f76eae80211177434cc5d1402c1254229c5
Geraldo Fada <geraldofada.neto@gmail.com>
2019-11-20 20:03:54 -0300

    Encoding and decoding testings

--- main.c
+++ main.c
@@ -28,25 +28,18 @@ void print2DUtil(hfm_Node *root, int space) {
 
 int main() {
     hfm_Tree *ht = hfm_Create();
-//
-//    hfm_Insert_Pool(ht, 19, 'a');
-//    hfm_Insert_Pool(ht, 1, 'b');
-//    hfm_Insert_Pool(ht, 2, 'c');
-//    hfm_Insert_Pool(ht, 8, 'd');
-//    hfm_Insert_Pool(ht, 99, 'e');
-//    hfm_Insert_Pool(ht, 7, 'f');
-//
-//    hfm_Gen_Tree(ht);
-//
-//    print2DUtil(ht->head, 1);
-//
-//    hfm_Destroy(ht);
     hfm_Insert_Pool_From_File(ht, "../probs.txt");
     hfm_Gen_Tree(ht);
-    print2DUtil(ht->head, 10);
-    for (int i = 0; i < ht->pool_size; i++){
-        printf("%s\n", ht->table[i]);
-    }
+
+    char *encoded = hfm_Encode_String(ht, "espaco");
+    printf("%s\n", encoded);
+
+    char *msg = hfm_Decode_String(ht, encoded);
+    printf("%s\n", msg);
+
+    free(msg);
+    free(encoded);
+
     hfm_Destroy(ht);
     return 0;
 }
