2001151490add4a84d74b9bd6ed6c62708badcc6
Geraldo Fada <geraldofada.neto@gmail.com>
2019-11-23 20:53:12 -0300

    New struct for btree info

--- b-tree/b-tree.c
+++ b-tree/b-tree.c
@@ -1,14 +1,15 @@
-#include <stdio.h>
 #include <stdlib.h>
 
 #include "b-tree.h"
 
 // PRIVATE
-bt_Node* bt_create_node(int t){
+bt_Node* bt_create_node(int t) {
     bt_Node* bn = (bt_Node *) malloc(sizeof(bt_Node));
     bn->nkey = 0;
-    bn->key = (int *) malloc(sizeof(int) * ((t * 2) - 1));
     bn->leaf = true;
+
+    bn->info = (bt_T_Info *) malloc(sizeof(bt_T_Info) * ((t * 2) - 1));
+
     bn->child = (bt_Node **) malloc(sizeof(bt_Node*) * t * 2);
 
     for (int i= 0; i < (t * 2); i++)
@@ -17,46 +18,48 @@ bt_Node* bt_create_node(int t){
     return bn;
 }
 
-void bt_destroy_node(bt_Node *bn){
+bt_Info bt_create_key_data();
+
+void bt_destroy_node(bt_Node *bn) {
     if (bn != NULL) {
         if (bn->child != NULL) {
             for (int i = 0; i <= bn->nkey; i++)
                 bt_destroy_node(bn->child[i]);
         }
 
-        free(bn->key);
+        free(bn->info);
         free(bn->child);
         free(bn);
     }
 }
 
-bt_Node* bt_search_node(bt_Node *bn, int key){
+bt_Node* bt_search_node(bt_Node *bn, bt_T_Info info) {
     bt_Node *found = NULL;
 
     if (bn == NULL)
         return found;
 
     int i = 0;
-    while (i < bn->nkey && key > bn->key[i])
+    while (i < bn->nkey && info.key > bn->info[i].key)
         i++;
 
-    if (i < bn->nkey && key == bn->key[i])
+    if (i < bn->nkey && info.key == bn->info[i].key)
         return bn;
 
     if (bn->leaf)
         return found;
 
-    return bt_search_node(bn->child[i], key);
+    return bt_search_node(bn->child[i], info);
 }
 
-bt_Node* bt_split_node(bt_Node *parent, int i, bt_Node *c_complete, int t){
+bt_Node* bt_split_node(bt_Node *parent, int i, bt_Node *c_complete, int t) {
     bt_Node *c_new = bt_create_node(t);
 
     c_new->nkey = t - 1;
     c_new->leaf = c_complete->leaf;
 
     for (int j = 0; j < t-1; j++)
-        c_new->key[j] = c_complete->key[j + t];
+        c_new->info[j] = c_complete->info[j + t];
 
     if (c_complete->leaf == false){
         for(int j = 0; j < t; j++){
@@ -72,50 +75,50 @@ bt_Node* bt_split_node(bt_Node *parent, int i, bt_Node *c_complete, int t){
     parent->child[i] = c_new;
 
     for(int j = parent->nkey; j >= i; j--)
-        parent->key[j] = parent->key[j - 1];
-    parent->key[i - 1] = c_complete->key[t - 1];
+        parent->info[j] = parent->info[j - 1];
+    parent->info[i - 1] = c_complete->info[t - 1];
 
     parent->nkey++;
 
     return parent;
 }
 
-bt_Node* bt_insert_node_incomplete(bt_Node *bn, int key, int t){
+bt_Node* bt_insert_node_incomplete(bt_Node *bn, bt_T_Info info, int t) {
     int i = bn->nkey - 1;
 
     if (bn->leaf == true) {
-        while ((i>=0) && (key < bn->key[i])) {
-            bn->key[i+1] = bn->key[i];
+        while ((i >= 0) && (info.key < bn->info[i].key)) {
+            bn->info[i+1] = bn->info[i];
             i--;
         }
 
-        bn->key[i+1] = key;
+        bn->info[i+1] = info;
         bn->nkey++;
 
         return bn;
     }
 
-    while ((i >= 0) && (key < bn->key[i]))
+    while ((i >= 0) && (info.key < bn->info[i].key))
         i--;
 
     i++;
     if (bn->child[i]->nkey == ((2 * t) - 1)) {
         bn = bt_split_node(bn, (i + 1), bn->child[i], t);
-        if (key > bn->key[i]) i++;
+        if (info.key > bn->info[i].key) i++;
     }
 
-    bn->child[i] = bt_insert_node_incomplete(bn->child[i], key, t);
+    bn->child[i] = bt_insert_node_incomplete(bn->child[i], info, t);
 
     return bn;
 }
 
-bt_Node* bt_insert_node(bt_Node *bn, int key, int t){
+bt_Node* bt_insert_node(bt_Node *bn, bt_T_Info key, int t) {
     if (bt_search_node(bn, key))
         return bn;
 
     if (bn == NULL) {
         bn = bt_create_node(t);
-        bn->key[0] = key;
+        bn->info[0] = key;
         bn->nkey = 1;
 
         return bn;
@@ -138,24 +141,20 @@ bt_Node* bt_insert_node(bt_Node *bn, int key, int t){
     return bn;
 }
 
-bt_Node* bt_remove_node_aux(bt_Node *bn, int key, int t){
+bt_Node* bt_remove_node_aux(bt_Node *bn, bt_T_Info info, int t) {
     if (bn == NULL)
         return NULL;
 
-    printf("Removendo %d...\n", key);
-
     int i;
-    for(i = 0; i < bn->nkey && bn->key[i] < key; i++);
+    for(i = 0; i < bn->nkey && bn->info[i].key < info.key; i++);
 
     //CASES: 1, 2A, 2B e 2C
-    if (i < bn->nkey && key == bn->key[i]){
+    if (i < bn->nkey && info.key == bn->info[i].key){
 
         // CASE 1
         if (bn->leaf == true){
-            printf("\nCASO 1\n");
-
             for (int j = i; j < bn->nkey-1; j++)
-                bn->key[j] = bn->key[j+1];
+                bn->info[j] = bn->info[j+1];
 
             bn->nkey--;
 
@@ -164,65 +163,59 @@ bt_Node* bt_remove_node_aux(bt_Node *bn, int key, int t){
 
         // CASE 2A
         if (bn->leaf == false && bn->child[i]->nkey >= t) {
-            printf("\nCASO 2A\n");
-
             bt_Node *aux = bn->child[i];  //Encontrar o predecessor k' de k na árvore com raiz em aux
 
             while (aux->leaf == false)
                 aux = aux->child[aux->nkey];
 
             //Eliminar recursivamente K e substitua K por K' em x
-            int temp = aux->key[aux->nkey-1];
+            bt_T_Info temp = aux->info[aux->nkey-1];
             bn->child[i] = bt_remove_node_aux(bn->child[i], temp, t);
-            bn->key[i] = temp;
+            bn->info[i] = temp;
 
             return bn;
         }
 
         // CASE 2B
         if (bn->leaf == false && bn->child[i+1]->nkey >= t) {
-            printf("\nCASO 2B\n");
-
             bt_Node *aux = bn->child[i+1];  //Encontrar o sucessor k' de k na árvore com raiz em aux
 
             while (aux->leaf == false)
                 aux = aux->child[0];
 
             //Eliminar recursivamente K e substitua K por K' em x
-            int temp = aux->key[0];
+            bt_T_Info temp = aux->info[0];
             aux = bt_remove_node_aux(bn->child[i + 1], temp, t);
-            bn->key[i] = temp;
+            bn->info[i] = temp;
 
             return bn;
         }
 
         // CASE 2C
         if (bn->leaf == false && bn->child[i+1]->nkey == t-1 && bn->child[i]->nkey == t-1) {
-            printf("\nCASO 2C\n");
-
             bt_Node *aux1 = bn->child[i];
             bt_Node *aux2 = bn->child[i+1];
 
-            aux1->key[aux1->nkey] = key;          //colocar key ao final de child[i]
+            aux1->info[aux1->nkey] = info;          //colocar info ao final de child[i]
 
             int j;
-            for (j=0; j<t-1; j++)                //juntar key[i+1] com key[i]
-                aux1->key[t+j] = aux2->key[j];
+            for (j=0; j<t-1; j++)                //juntar info[i+1] com info[i]
+                aux1->info[t+j] = aux2->info[j];
 
             for (j=0; j<=t; j++)                 //juntar child[i+1] com child[i]
                 aux1->child[t+j] = aux2->child[j];
 
             aux1->nkey = 2*t-1;
 
-            for (j=i; j < bn->nkey-1; j++)   //bt_remove_node_aux key de bn
-                bn->key[j] = bn->key[j+1];
+            for (j=i; j < bn->nkey-1; j++)   //bt_remove_node_aux info de bn
+                bn->info[j] = bn->info[j+1];
 
             for (j=i+1; j <= bn->nkey; j++)  //bt_remove_node_aux ponteiro para child[i+1]
                 bn->child[j] = bn->child[j+1];
 
             bn->child[j] = NULL;
             bn->nkey--;
-            bn->child[i] = bt_remove_node_aux(bn->child[i], key, t);
+            bn->child[i] = bt_remove_node_aux(bn->child[i], info, t);
 
             return bn;
         }
@@ -235,68 +228,61 @@ bt_Node* bt_remove_node_aux(bt_Node *bn, int key, int t){
 
         // CASE 3A
         if ((i < bn->nkey) && (bn->child[i+1]->nkey >= t)) {
-            printf("\nCASO 3A: i menor que nkey\n");
-
             z = bn->child[i+1];
-            y->key[t-1] = bn->key[i];   //dar a y a key i da bn
+            y->info[t-1] = bn->info[i];   //dar a y a info i da bn
             y->nkey++;
 
-            bn->key[i] = z->key[0];     //dar a bn uma key de z
+            bn->info[i] = z->info[0];     //dar a bn uma info de z
 
             int j;
             for (j=0; j < z->nkey-1; j++)  //ajustar chaves de z
-                z->key[j] = z->key[j+1];
+                z->info[j] = z->info[j+1];
 
-            //z->key[j] = 0; Rosseti
+            //z->info[j] = 0; Rosseti
             y->child[y->nkey] = z->child[0]; //enviar ponteiro menor de z para o novo elemento em y
             for (j=0; j < z->nkey; j++)       //ajustar filhos de z
                 z->child[j] = z->child[j+1];
 
             z->nkey--;
-            bn->child[i] = bt_remove_node_aux(bn->child[i], key, t);
+            bn->child[i] = bt_remove_node_aux(bn->child[i], info, t);
 
             return bn;
         }
 
         // CASE 3A
         if ((i > 0) && (!z) && (bn->child[i-1]->nkey >= t)) {
-            printf("\nCASO 3A: i igual a nkey\n");
-
             z = bn->child[i-1];
 
             int j;
-            for (j = y->nkey; j>0; j--)               //encaixar lugar da nova key
-                y->key[j] = y->key[j-1];
+            for (j = y->nkey; j>0; j--)               //encaixar lugar da nova info
+                y->info[j] = y->info[j-1];
 
-            for (j = y->nkey+1; j>0; j--)             //encaixar lugar dos filhos da nova key
+            for (j = y->nkey+1; j>0; j--)             //encaixar lugar dos filhos da nova info
                 y->child[j] = y->child[j-1];
 
-            y->key[0] = bn->key[i-1];              //dar a y a key i da bn
+            y->info[0] = bn->info[i-1];              //dar a y a info i da bn
             y->nkey++;
 
-            bn->key[i-1] = z->key[z->nkey-1];   //dar a bn uma key de z
+            bn->info[i-1] = z->info[z->nkey-1];   //dar a bn uma info de z
 
             y->child[0] = z->child[z->nkey];         //enviar ponteiro de z para o novo elemento em y
             z->nkey--;
 
-            bn->child[i] = bt_remove_node_aux(y, key, t);
+            bn->child[i] = bt_remove_node_aux(y, info, t);
 
             return bn;
         }
 
         // CASE 3B
         if (!z) {
-
             if (i < bn->nkey && bn->child[i+1]->nkey == t-1){
-                printf("\nCASO 3B: i menor que nkey\n");
-
                 z = bn->child[i+1];
-                y->key[t-1] = bn->key[i];     //pegar key [i] e coloca ao final de child[i]
+                y->info[t-1] = bn->info[i];     //pegar info [i] e coloca ao final de child[i]
                 y->nkey++;
 
                 int j;
                 for(j=0; j < t-1; j++){
-                    y->key[t+j] = z->key[j];     //passar child[i+1] para child[i]
+                    y->info[t+j] = z->info[j];     //passar child[i+1] para child[i]
                     y->nkey++;
                 }
 
@@ -307,30 +293,28 @@ bt_Node* bt_remove_node_aux(bt_Node *bn, int key, int t){
                 }
 
                 for(j=i; j < bn->nkey-1; j++){ //limpar referências de i
-                    bn->key[j] = bn->key[j+1];
+                    bn->info[j] = bn->info[j+1];
                     bn->child[j+1] = bn->child[j+2];
                 }
 
                 bn->nkey--;
-                bn = bt_remove_node_aux(bn, key, t);
+                bn = bt_remove_node_aux(bn, info, t);
 
                 return bn;
             }
 
             if ((i > 0) && (bn->child[i-1]->nkey == t-1)){
-                printf("\nCASO 3B: i igual a nkey\n");
-
                 z = bn->child[i-1];
 
                 if(i == bn->nkey)
-                    z->key[t-1] = bn->key[i-1]; //pegar key[i] e poe ao final de child[i-1]
+                    z->info[t-1] = bn->info[i-1]; //pegar info[i] e poe ao final de child[i-1]
                 else
-                    z->key[t-1] = bn->key[i];   //pegar key [i] e poe ao final de child[i-1]
+                    z->info[t-1] = bn->info[i];   //pegar info [i] e poe ao final de child[i-1]
 
                 z->nkey++;
                 int j;
                 for(j=0; j < t-1; j++){
-                    z->key[t+j] = y->key[j];     //passar child[i+1] para child[i]
+                    z->info[t+j] = y->info[j];     //passar child[i+1] para child[i]
                     z->nkey++;
                 }
 
@@ -342,19 +326,19 @@ bt_Node* bt_remove_node_aux(bt_Node *bn, int key, int t){
 
                 bn->nkey--;
                 bn->child[i-1] = z;
-                bn = bt_remove_node_aux(bn, key, t);
+                bn = bt_remove_node_aux(bn, info, t);
 
                 return bn;
             }
         }
     }
 
-    bn->child[i] = bt_remove_node_aux(bn->child[i], key, t);
+    bn->child[i] = bt_remove_node_aux(bn->child[i], info, t);
 
     return bn;
 }
 
-bt_Node* bt_remove_node(bt_Node *bn, int key, int t){
+bt_Node* bt_remove_node(bt_Node *bn, bt_T_Info key, int t) {
     if (bn == NULL || bt_search_node(bn, key) == NULL)
         return bn;
 
@@ -370,16 +354,16 @@ bt_Tree* bt_Create(int t) {
     return bt;
 }
 
-void bt_Insert(bt_Tree *bt, int key) {
-    bt->head = bt_insert_node(bt->head, key, bt->t);
+void bt_Insert(bt_Tree *bt, bt_T_Info info) {
+    bt->head = bt_insert_node(bt->head, info, bt->t);
 }
 
-void bt_Remove(bt_Tree *bt, int key) {
-    bt->head = bt_remove_node(bt->head, key, bt->t);
+void bt_Remove(bt_Tree *bt, bt_T_Info info) {
+    bt->head = bt_remove_node(bt->head, info, bt->t);
 }
 
-bt_Node* bt_Search(bt_Tree *bt, int key) {
-    return bt_search_node(bt->head, key);
+bt_Node* bt_Search(bt_Tree *bt, bt_T_Info info) {
+    return bt_search_node(bt->head, info);
 }
 
 void bt_Destroy(bt_Tree* bt) {
--- b-tree/b-tree.h
+++ b-tree/b-tree.h
@@ -5,26 +5,36 @@
 
 typedef struct bt_tree bt_Tree;
 typedef struct bt_node bt_Node;
+typedef struct bt_info bt_Info;
 
-struct bt_tree{
+typedef bt_Info bt_T_Info;
+
+struct bt_tree {
     int t;
     bt_Node *head;
 };
 
-struct bt_node{
-    int nkey, *key;
+struct bt_node {
+    int nkey;
     bool leaf;
 
+    bt_T_Info *info;
+
     struct bt_node **child;
+};
+
+struct bt_info {
+    char key;
+    float prob;
 };
 
 
 bt_Tree* bt_Create(int t);
 
-void bt_Insert(bt_Tree *bt, int key);
-void bt_Remove(bt_Tree *bt, int key);
+void bt_Insert(bt_Tree *bt, bt_T_Info info);
+void bt_Remove(bt_Tree *bt, bt_T_Info info);
 
-bt_Node* bt_Search(bt_Tree *bt, int key);
+bt_Node* bt_Search(bt_Tree *bt, bt_T_Info info);
 
 void bt_Destroy(bt_Tree* bt);
 
